64c53dc18020a3b9bc817dd4577a964fdf466be8
surajpilla242 <164089032+surajpilla242@users.noreply.github.com>
2025-03-05 12:33:31 -0500

    Update suraj.py

--- suraj.py
+++ suraj.py
@@ -1,1 +1,46 @@
+def 
 
+
+// FUNCTION wavefront algorithm to find most efficient path to goal
+void WavefrontSearch()
+{
+  int goal_x, goal_y;
+  bool foundWave = true;
+  int currentWave = 2; // Looking for goal first
+
+  while(foundWave == true)
+  {
+    foundWave = false;
+    for(int y=0; y < y_size; y++)
+    {
+      for(int x=0; x < x_size; x++)
+      {
+        if(map[x][y] == currentWave)
+        {
+          foundWave = true;
+          goal_x = x;
+          goal_y = y;
+
+          if(goal_x > 0)
+            if(map[goal_x-1][goal_y] == 0)
+              map[goal_x-1][goal_y] = currentWave + 1;
+
+          if(goal_x < (x_size - 1))
+            if(map[goal_x+1][goal_y] == 0)
+              map[goal_x+1][goal_y] = currentWave + 1;
+
+          if(goal_y > 0)
+            if(map[goal_x][goal_y-1] == 0)
+              map[goal_x][goal_y-1] = currentWave + 1;
+
+          if(goal_y < (y_size - 1))
+            if(map[goal_x][goal_y+1] == 0)
+              map[goal_x][goal_y+1] = currentWave + 1;
+        }
+      }
+    }
+    currentWave++;
+    PrintWavefrontMap();
+    wait1Msec(500);
+  }
+}
