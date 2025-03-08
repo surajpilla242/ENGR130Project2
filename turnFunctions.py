8bf92e7d8a77bb420d4514a9b3bfcaa577ea5788
sarapineda-m <sarapineda1414@gmail.com>
2025-03-08 00:00:14 -0500

    Update turnFunctions.py

--- turnFunctions.py
+++ turnFunctions.py
@@ -1,52 +1,29 @@
 from microbit import *
-import radio
 import robotbit_library as r
 
-# Define Ports for the bank of high current output
+# port definitions.
 M1A = 0x1
-M1B = 0x2
-M2A = 0x3
 M2B = 0x4
 
-def Drive(lft,rgt):
-# Receive the percent power to drive each motor in a specific direction
-    r.motor(M2B, lft)
-    r.motor(M1A, rgt)
+# sets up the robotbit
+r.setup()     
 
+#left 90 turn function
 def turnLeft90():
-  #distance one wheel must travel for 90 degree point turn = 8.6 cm
-  # wheel circumference = 19.5 cm
-    countsToTravel = ((8.6/17.6)/19.5)*(360)
+    r.motor(M1A,100) #right motor
+    r.motor(M2B,100) #left motor
+    sleep(250) #duration of turn (0.25 sec)
+    r.motor(M1A,0) #stop right motor
+    r.motor(M2B,0) #stop left motor
 
-  #encoder target for countsToTravel
-    r.motor(M2B, 0)
-    r.motor(M1A, 0)
-    r.motor(M2B, countsToTravel)
-    r.motor(M1A, countsToTravel)
-    r.motor(M2B, 50)
-    r.motor(M1A, -50)
-    while(r.motor.status(M2B) < countsToTravel and r.motor.status(M1A) < countsToTravel):
-        r.motor(M2B, 50)
-        r.motor(M1A, -50)
+#right 90 turn function
+def turnRight90():
+    r.motor(M1A,-100) #right motor
+    r.motor(M2B,-100) #left motor
+    sleep(250) #duration of turn (0.25 sec)
+    r.motor(M1A,0) #stop right motor
+    r.motor(M2B,0) #stop left motor
 
-    
-#stop for half second at end of movement
-    r.motor(M2B, 0)
-    r.motor(M1A, 0)
-    sleep(500)
-
-def turnright90():
-    #wheel_travel_distance = 8.6 
-    #wheel_circumference = 19.5  
-    countsToTravel = ((8.6/17.6)/19.5)*(360)
-    
-    r.motor(M2B, 0)
-    r.motor(M1A, 0)
-    r.motor(M2B, countsToTravel)
-    r.motor(M1A, countsToTravel)
-    r.motor(M2B, -50) 
-    r.motor(M1A, 50)   
-   
-    r.motor(M2B, 0)
-    r.motor(M1A, 0)
-    sleep(500)
+turnLeft90()
+sleep(5000) #pause for 5 seconds 
+turnRight90()
