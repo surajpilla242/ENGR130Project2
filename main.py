78f5f86975aed283788085e219fdf3cbfc326881
KeanTian <157336931+KeanTian@users.noreply.github.com>
2025-03-09 14:39:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -42,8 +42,7 @@ def moveForward(numBlocks):
     r.motor(M1A,0) #stop right motor
     r.motor(M2B,0) #stop left motor
 
-
-with open('test.txt', 'r') as file:
+""" with open('test.txt', 'r') as file:
   lines = [line.strip() for line in file.readlines()]
   
 for i in range(6)
@@ -51,3 +50,68 @@ for i in range(6)
       if lines[i][j] == 'G'
         x_goal = i
         y_goal = j
+"""
+def wavefrontSearch():
+    current_wave = 2
+    found_wave = True
+    while found_wave:
+        found_wave = False
+        for y in range(y_size):
+            for x in range(x_size):
+                if grid_map[x][y] == current_wave:
+                    found_wave = True
+                    for dx, dy in [(-1,0),(1,0),(0,-1),(0,1)]:
+                        nx, ny = x+dx, y+dy
+                        if 0<=nx<x_size and 0<=ny<y_size:
+                            if grid_map[nx][ny] == 0:
+                                grid_map[nx][ny] = current_wave + 1
+        current_wave += 1
+        time.sleep_ms(500)  
+
+def navigateToGoal():
+    robot_pos = None
+    for x in range(x_size):
+        for y in range(y_size):
+            if grid_map[x][y] == 99:
+                robot_pos = (x, y)
+                break
+        if robot_pos: break
+    
+    current_dir = 0  # 0=North,1=East,2=South,3=West
+    x, y = robot_pos
+    
+    while True:
+        min_val = 99
+        next_dir = current_dir
+        next_pos = (x, y)
+        
+        for dir, (dx, dy) in enumerate([(0,1),(1,0),(0,-1),(-1,0)]):
+            nx, ny = x+dx, y+dy
+            if 0<=nx<x_size and 0<=ny<y_size:
+                if grid_map[nx][ny] < min_val and grid_map[nx][ny] >=2:
+                    min_val = grid_map[nx][ny]
+                    next_pos = (nx, ny)
+                    next_dir = dir
+        
+        dir_diff = (next_dir - current_dir) % 4
+        if dir_diff == 1:
+            turnRight90()
+        elif dir_diff == 3:
+            turnLeft90()
+        elif dir_diff == 2:
+            turnRight90()
+            turnRight90()
+            
+        moveForward(1)
+        x, y = next_pos
+        current_dir = next_dir
+        
+        if grid_map[x][y] == 2:
+            break
+            
+time.sleep(1000)
+wavefrontSearch()
+navigateToGoal()
+
+while True:
+    time.sleep(1000)
