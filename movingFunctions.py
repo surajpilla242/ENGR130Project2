e3e8c8c416779eec68595c87649b1e2317c661eb
sarapineda-m <sarapineda1414@gmail.com>
2025-03-09 18:28:45 -0400

    Update and rename turnFunctions.py to movingFunctions.py

--- movingFunctions.py
+++ movingFunctions.py
@@ -6,11 +6,11 @@ M1A = 0x1
 M2B = 0x4
 
 # sets up the robotbit
-r.setup()     
+r.setup()  
 
 #left 90 turn function
 def turnRight90():
-    powerSetting = [100,0]    # list of power settings, in pct, to cycle through
+    powerSetting = [1-0,0]    # list of power settings, in pct, to cycle through
     for pwr_sig in powerSetting:
         r.motor(M1A, pwr_sig)
         r.motor(M2B, pwr_sig)
@@ -24,6 +24,16 @@ def turnLeft90():
         r.motor(M2B, pwr_sig)
         sleep(250) 
 
-turnLeft90()
-sleep(5000) #pause for 5 seconds 
-turnRight90()
+#forward function
+def move_forward():    
+    powerSetting = [35,0]    # list of power settings, in pct, to cycle through
+    for pwr_sig in powerSetting:
+        r.motor(M1A,-1*pwr_sig)
+        r.motor(M2B,1.1*pwr_sig)
+        print("Power signal = {}".format(pwr_sig))
+        sleep(1600) 
+
+while True :
+    turnLeft90()
+    turnRight90()
+    move_forward()
