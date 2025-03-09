c2986c8e6a908a837d1672b1ff7939f1439faf18
sarapineda-m <sarapineda1414@gmail.com>
2025-03-09 14:34:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -41,21 +41,6 @@ def moveForward(numBlocks):
     sleep(3*duration)
     r.motor(M1A,0) #stop right motor
     r.motor(M2B,0) #stop left motor
-    
-#ultrasonic function
-def distance(tp, ep):
-    # tp is the trigger pin and ep is the echo pin
-    ep.read_digital()  # clear echo
-    tp.write_digital(1)  # Send a 10 microSec pulse
-    sleep_us(10)  # wait 10 microSec
-    tp.write_digital(0)  # Send pulse low
-    ep.read_digital()  # clear echo signal - This is needed for a
-    # pingSensor
-    ts = time_pulse_us(ep, 1, TIME_OUT)  # Wait for echo or time out
-    if ts > 0:
-        ts = ts * 17 // 100  # if system did not timeout, then send
-        # back a scaled value
-    return ts  # Return timeout error as a negative number (-1)
 
 
 with open('test.txt', 'r') as file:
